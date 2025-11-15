636cb99f6d9140bc22e8d297f0eb3f45445ae4c5
Robert MIZIELSKI <robert.mizielski@gmail.com>
2025-11-15 23:33:44 +0100

    Create DifficultiesEnum.cs

---
+++ Assets/Scripts/Enums/DifficultiesEnum.cs
@@ -0,0 +1,10 @@
+namespace Physiqia
+{
+    public class DifficultiesEnum
+    {
+        public const byte INTRO = 0;
+        public const byte EASY = 1;
+        public const byte MEDIUM = 2;
+        public const byte HARD = 3;
+    }
+}
