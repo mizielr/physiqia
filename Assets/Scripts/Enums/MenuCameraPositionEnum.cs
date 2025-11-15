535eb8eda4a13cad8b95f6a3c79ce7cd6ec4e163
Robert MIZIELSKI <robert.mizielski@gmail.com>
2025-11-15 23:35:57 +0100

    Create MenuCameraPositionEnum.cs

---
+++ Assets/Scripts/Enums/MenuCameraPositionEnum.cs
@@ -0,0 +1,13 @@
+namespace Physiqia
+{
+    public class MenuCameraPositionEnum
+    {
+        public const string KEY = "_menuCamPos";
+        public const byte DEFAULT = 0;
+        public const byte QUIZ = 1;
+        public const byte THE_LAB = 2;
+        public const byte THE_SPACE_CENTER = 3;
+        public const byte EXPEDITIONS = 4;
+        public const byte MINIGAMES = 5;
+    }
+}
