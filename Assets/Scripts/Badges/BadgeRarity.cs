a60053e203bcf9b21b869212792d477b704c7262
Robert MIZIELSKI <robert.mizielski@gmail.com>
2026-08-18 22:08:43 +0200

    Create BadgeRarity.cs

---
+++ Assets/Scripts/Badges/BadgeRarity.cs
@@ -0,0 +1,12 @@
+namespace Physiqia.Badges
+{
+    public enum BadgeRarity
+    {
+        Common = 1,
+        Uncommon = 2,
+        Rare = 3,
+        Epic = 4,
+        Legendary = 5,
+        Developer = 8,
+    }
+}
