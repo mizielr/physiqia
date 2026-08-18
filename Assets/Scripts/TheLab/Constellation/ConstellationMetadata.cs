fe03fd06489c021b4d3e10a0acce5016fe14519e
Robert MIZIELSKI <robert.mizielski@gmail.com>
2026-08-18 22:31:13 +0200

    Create ConstellationMetadata.cs

---
+++ Assets/Scripts/TheLab/Constellation/ConstellationMetadata.cs
@@ -0,0 +1,17 @@
+namespace Physiqia.TheLab.Constellation
+{
+    public class ConstellationMetadata
+    {
+        public string Hemisphere { get; set; }
+        public string Season { get; set; }
+        public string Group { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string CommonName { get; set; }
+        public float BrightestMagnitude { get; set; }
+        public float Area { get; set; }
+        public int MainStars { get; set; }
+        public int BayerStars { get; set; }
+        public string BrightestStar { get; set; }
+    }
+}
